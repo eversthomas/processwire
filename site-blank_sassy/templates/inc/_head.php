6be6431cee38fae9e283b0048bdbe174c9913615
Thomas <evers.thomas@gmail.com>
2021-03-05 19:58:19 +0100

    add new site profile
    a blank site profile using sass

---
+++ site-blank_sassy/templates/inc/_head.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="de"> <!-- Set this to the main language of your site -->
+<head>
+    <meta charset="utf-8">
+
+    <title><?php echo $page->title; ?></title>
+
+    <!-- Enter a brief description of your page -->
+    <meta name="description" content="">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <!-- Include the site stylesheet -->
+    <link rel="stylesheet" type="text/css" href="<?php echo Sassify::css('styles/sass/style.scss'); ?>">
+
+    <!-- Include the HTML5 shiv print polyfill for Internet Explorer browsers 8 and below -->
+    <!--[if lt IE 9]><script src="js/html5shiv-printshiv.js" media="all"></script><![endif]-->
+</head>
+<body>
+	
+	<header role="banner">
+		<h1><?php echo $page->title; ?></h1>
+
+        <nav role="navigation" id="site-navigation" class="main-navigation">
+			<button class="menu-toggle hamburger hamburger--spin" aria-controls="primary-menu" aria-expanded="false">
+				<span class="hamburger-box">
+					<span class="hamburger-inner"></span>
+				</span>
+			</button>
+			<div class="wrapper">
+				<?php
+				$treeMenu = $modules->get("MarkupSimpleNavigation"); // load the module
+				echo $treeMenu->render(); // render default menu
+				?>
+			</div>
+		</nav><!-- #site-navigation -->
+
+    </header>
+	
+	<div class="page with-sidebar">
