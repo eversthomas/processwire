da4fe6f8d077ce45a3b576da7de3c8063af6e2de
Thomas <evers.thomas@gmail.com>
2021-03-13 11:19:34 +0100

    ..

--- site-blog-editorial/templates/blog-rss.php
+++
@@ -1,5 +0,0 @@
-
-					<section>
-						<p><?= _x("Subscribe to", "blog") ?> <a href="<?= $pages->get('/posts/')->url ?>rss/"><span class="icon fa-feed"> </span> <?= $page->title ?></a></p>
-					</section>
-
